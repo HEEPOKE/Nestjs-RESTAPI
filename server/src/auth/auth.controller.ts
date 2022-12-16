501c91b4e53ef7fde77c708df7c5c53ac408fffb
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-17 03:30:52 +0700

    update auth

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
