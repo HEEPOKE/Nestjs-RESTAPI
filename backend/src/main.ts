e5e2904674b96d5ba46d4ebc236212a276ce8445
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-16 15:47:20 +0700

    update orm

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(6476);
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('NESTJS')
+    .setDescription('NESTJS API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,19 +1,51 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import * as bcrypt from 'bcrypt';
 
-@Entity()
+@Entity({ name: 'users' })
 export class User {
+  @ApiProperty({ description: 'Primary key as User ID', example: 1 })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ description: 'User name', example: 'Jhon Doe' })
   @Column()
   Username: string;
 
-  @Column()
+  @ApiProperty({
+    description: 'User email address',
+    example: 'jhon.doe@gmail.com',
+  })
+  @Column({
+    unique: true,
+  })
   Email: string;
 
+  @ApiProperty({ description: 'Hashed user password' })
   @Column()
   Password: string;
 
   @Column()
   Role: string;
+
+  @ApiProperty({ description: 'When user was created' })
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty({ description: 'When user was updated' })
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async setPassword(password: string) {
+    const salt = await bcrypt.genSalt();
+    this.Password = await bcrypt.hash(password || this.Password, salt);
+  }
 }
