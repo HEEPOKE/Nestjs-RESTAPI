95d311ff1eab5816a8dd458782e9e7f900dd2d0a
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-16 10:38:14 +0700

    Update users.controller.ts

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -35,8 +35,8 @@ export class UsersController {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.usersService.remove(+id);
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
 }
