c1c3fc8a95ac52546dbd9ac943a123333ece69be
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-17 02:50:12 +0700

    update eslint

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
