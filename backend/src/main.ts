ca047209d3f6285862190cce86dda03d1768bb6d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-16 12:03:15 +0700

    update validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(
-    new ValidationPipe({
-      disableErrorMessages: true,
-    }),
-  );
   await app.listen(6476);
 }
 bootstrap();
--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -2,15 +2,15 @@ import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 export class CreateUserDto {
   id: number;
 
-  // @IsNotEmpty()
+  @IsNotEmpty()
   username: string;
 
-  // @IsNotEmpty()
-  // @IsEmail()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
-  // @IsNotEmpty()
-  // @Length(8, 20)
+  @IsNotEmpty()
+  @Length(8, 20)
   password: string;
 
   role: string;
