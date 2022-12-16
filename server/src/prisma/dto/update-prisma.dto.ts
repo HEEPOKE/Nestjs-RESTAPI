411a8026d67640b625f8c99ce74309bc76e45729
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-17 05:18:31 +0700

    export prisma

--- server/src/prisma/dto/update-prisma.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePrismaDto } from './create-prisma.dto';
-
-export class UpdatePrismaDto extends PartialType(CreatePrismaDto) {}
--- server/src/prisma/prisma.module.ts
+++ server/src/prisma/prisma.module.ts
@@ -1,10 +1,8 @@
 import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { PrismaController } from './prisma.controller';
-
 @Global()
 @Module({
-  controllers: [PrismaController],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
