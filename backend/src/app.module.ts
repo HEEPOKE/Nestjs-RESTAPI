d9e31002687e351dca0b862e6add65d25ee95fe3
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-16 17:34:41 +0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,23 +4,16 @@ import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      // host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME || 'root',
-      password: `${process.env.DB_PASSWORD}`,
-      database: process.env.DB_NAME,
-      entities: [User],
-      // entities: [__dirname + '/../**/*.entity.{js,ts}'],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    EventEmitterModule.forRoot(),
     UsersModule,
     ProductModule,
   ],
