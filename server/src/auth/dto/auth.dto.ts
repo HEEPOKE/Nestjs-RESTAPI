13f5c00bf97d691472be1e813e2e3d04f5bdcf73
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-18 02:31:25 +0700

    validation

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,5 +1,16 @@
 /* eslint-disable prettier/prettier */
-export interface AuthDto {
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
