431d715751b0364ecc81d0dc8bce79605f751d3d
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-18 22:41:02 +0700

    set bcript

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as bycrypt from 'bcrypt';
 import { AuthDto } from './dto';
 
 @Injectable()
