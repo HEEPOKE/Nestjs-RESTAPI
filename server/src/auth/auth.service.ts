8053ea1a949b48b2dc70cb792bdcd8390ec48e73
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-18 22:44:30 +0700

    Update auth.service.ts

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import * as bycrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { AuthDto } from './dto';
 
 @Injectable()
@@ -8,6 +8,11 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   signUp(dto: AuthDto) {
+    const salt = bcrypt.genSalt<string>(10);
+    const hashBcrypt = bcrypt.hash(
+      dto.password,
+      salt,
+    );
     return { msg: dto };
   }
 
