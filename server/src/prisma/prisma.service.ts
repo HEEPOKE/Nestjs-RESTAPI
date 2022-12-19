c48ed58eeb080f10a0befed580c4406f5fd44425
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-19 11:52:43 +0700

    config database

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://yoyo:5555@localhost:5434/prisma?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
