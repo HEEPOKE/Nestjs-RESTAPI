b3a2090f2822f95b1929b569bd59261d2b1a639b
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-19 10:44:20 +0700

    Update prisma.service.ts

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  // user: any;
   constructor() {
     super({
       datasources: {
