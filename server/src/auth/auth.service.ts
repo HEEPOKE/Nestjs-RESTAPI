c72f114e265de3be71c1bd1e3e84161b8e6c6887
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-19 11:19:52 +0700

    update model

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -13,15 +17,28 @@ export class AuthService {
       dto.password,
       salt,
     );
-    const newUser = await this.prisma.user.create(
-      {
-        data: {
-          email: dto.email,
-          password: hash,
-        },
-      },
-    );
-    return newUser;
+    try {
+      const newUser =
+        await this.prisma.user.create({
+          data: {
+            email: dto.email,
+            password: hash,
+          },
+        });
+      return newUser;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
   signIn() {
